2ca5fa08725c5e487fadda7694a58aee7d500239
praful247 <spraful339@gmail.com>
2025-06-17 10:04:05 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+
+
+
+app.get('/',(req ,res)=>{
+  res.send({hi : 'there'});
+});
+
+const PORT = process.env.PORT||3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
