6c3bb2f8989e3577a05c2412c81349c5a605c9ac
praful247 <spraful339@gmail.com>
2025-06-20 18:16:28 +0530

    callback url

--- services/passport.js
+++ services/passport.js
@@ -16,7 +16,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
      clientID : keys.googleClientID,
      clientSecret : keys.googleClientSecret,
-     callbackURL : 'https://emailandpayment-2.onrender.com/auth/google/callback',
+     callbackURL : '/auth/google/callback',
      proxy:true
 }, (accessToken, refreshToken, profile, done) => {
   User.findOne({googleID : profile.id})
